7b066f5463a4f3502feb3468649012aef9f8b1ef
Abel Svoboda <abel465@gmail.com>
2024-06-26 15:42:43 +1200

    Add nix run capability

--- flake.nix
+++ flake.nix
@@ -12,7 +12,17 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-    in {
+    in rec {
+      abelscreensaver = pkgs.rustPlatform.buildRustPackage {
+        pname = "abelscreensaver";
+        version = "0.0.0";
+        src = ./.;
+        cargoHash = "sha256-lK7g0j0cObSP1/rVM1jd402OJAFHo6psOutsSDkK33w=";
+      };
+      apps.default = {
+        type = "app";
+        program = "${abelscreensaver}/bin/abelscreensaver";
+      };
       devShell = with pkgs;
         mkShell {
           buildInputs = [
