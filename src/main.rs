9b929a430d5b2d1b1e2e3ce079a2d70d137cade2
Abel Svoboda <abel465@gmail.com>
2024-08-08 13:24:44 +1200

    Inline black pixel

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,27 @@ mod settings;
 use crate::settings::Options;
 use std::fs::File;
 use std::io::prelude::Write;
+use std::path::PathBuf;
 
 fn main() {
+    ensure_ffprobe_exists();
+    runner::run(Options::load(), black_pixel_path());
+}
+
+fn ensure_ffprobe_exists() {
     if let Err(ffprobe::FfProbeError::Io(err)) = ffprobe::ffprobe("") {
         if err.kind() == std::io::ErrorKind::NotFound {
             panic!("ffprobe: command not found");
         }
     }
+}
+
+/// Writes a black pixel to the temp filesystem and returns the path
+fn black_pixel_path() -> PathBuf {
     let temp_dir = std::env::temp_dir().join("abel_screensaver/");
     std::fs::create_dir_all(&temp_dir).unwrap();
     let black_pixel_path = temp_dir.join("black_pixel.pbm");
     let mut file = File::create(&black_pixel_path).unwrap();
-    file.write_all(std::include_bytes!("../assets/black_pixel.pbm"))
-        .unwrap();
-    let opts = Options::load();
-    runner::run(opts, black_pixel_path);
+    file.write_all(b"P1\n1 1\n1").unwrap();
+    black_pixel_path
 }
